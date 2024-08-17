b0e06ded04aac6b5bcfd978164e4247f382d26ff
Byakkuya <contact.ahmedzbaa@gmail.com>
2024-08-17 22:48:02 +0100

    added a dateRangepicker

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { UserSettings } from '@prisma/client'
+import { startOfMonth } from 'date-fns';
+import React, { useState } from 'react'
+
+function Overview({userSettings}:{userSettings:UserSettings}) {
+    const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+        from: startOfMonth (new Date()),
+        to: new Date(),
+        });
+        
+    return (
+        <>
+        <div className="container flex flex-wrap items-end justify-between gap-2 py-6">
+            <h2 className="text-3xl font-bold">Overview</h2>
+            <div className="flex items-center gap-3">
+                <DateRangePicker 
+                    initialCompareFrom={dateRange.from}
+                    initialCompareTo={dateRange.to}
+                    showCompare={false}
+                    onUpdate={(values) => {
+                        const {from,to} = values.range;
+                    // we update the date range only if both from and to are set
+                        if (!from || !to) return;
+
+                    }}
+                />
+            </div>
+        </div>
+        </>
+  )
+}
+
+export default Overview
