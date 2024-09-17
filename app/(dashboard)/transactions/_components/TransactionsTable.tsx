d6d4b95b93ec98c9a069c71411983719f9dd8982
Byakkuya <contact.ahmedzbaa@gmail.com>
2024-09-17 22:16:04 +0100

    working on the table

--- app/(dashboard)/transactions/_components/TransactionsTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionsTable.tsx
@@ -1,11 +1,15 @@
 "use client"
 import { DateToUTC } from '@/lib/helpers'
 import { useQuery } from '@tanstack/react-query'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import {
     ColumnDef,
+    ColumnFiltersState,
     flexRender,
     getCoreRowModel,
+    getFacetedRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
     getSortedRowModel,
     SortingState,
     useReactTable,
@@ -21,6 +25,10 @@ import {
 import SkeletonWrapper from '@/components/SkeletonWrapper'
 import { DataTableColumnHeader } from '@/components/datatable/ColumnHeader'
 import { cn } from '@/lib/utils'
+import { Value } from '@radix-ui/react-select'
+import { DataTableFacetedFilter } from '@/components/datatable/FacetedFilters'
+import { DataTableViewOptions } from '@/components/datatable/ColumnToggle'
+import { Button } from '@/components/ui/button'
 
 interface Props {
     from: Date
@@ -44,6 +52,16 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
     header: ({column}) => (
       <DataTableColumnHeader column={column} title="Category" />
     ),
+
+    filterFn: (row, id, value) => {
+      // If value is an array (for multiple selections), use `.some()` to check if the row's value is included
+      if (Array.isArray(value)) {
+        return value.some(v => v === row.getValue(id));
+      }
+      // Otherwise, it's a single value, so check for equality
+      return row.getValue(id) === value;
+    },
+    
     cell: ({ row }) => {
       const data = row.original as TransactionHistoryRow;
       console.log(data)
@@ -110,6 +128,9 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
     header: ({column}) => (
       <DataTableColumnHeader column={column} title="Amount" />
     ),
+    filterFn: (row,id,value) => {
+      return value.includes(row.getValue(id))
+    },
     cell: ({ row }) => (
       <p className="text-md rounded-lg bg-gray-400/5 p-2 text-center">
         {/* @ts-ignore */}
@@ -122,6 +143,7 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
 
 function TransactionsTable({ from, to }: Props) {
   const [sorting,setSorting] = useState<SortingState>([])
+  const [columnFilters,setColumnFilters] = useState<ColumnFiltersState>([])
   const history = useQuery<TransactionHistoryRow[]>({
     queryKey: ["transactions", "history", from, to],
     queryFn: () => fetch(`/api/transactions-history?from=${DateToUTC(from)}&to=${DateToUTC(to)}`).then((res) => res.json())
@@ -131,16 +153,46 @@ function TransactionsTable({ from, to }: Props) {
     data: history.data || emptyData,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 2,
+      }
+    },
     state: {
       sorting,
-    },
+      columnFilters,
+    },  
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
+  
 
+  const categoriesOptions = useMemo(() => {
+    const categoriesMap = new Map();
+    history.data?.forEach((transaction) => {
+      categoriesMap.set(transaction.category, {
+        Value: transaction.category,
+        label: `${transaction.categoryIcon} (${transaction.category})`,
+      });
+    });
+    const uniqueCategories = new Set(categoriesMap.values());
+    return Array.from(uniqueCategories);
+    
+  }, [history.data])
   return (
    <div className=" w-full">
-    <div className="flex flex-wrap items-end justify-between gap-2 py-4"></div>
+    
+    <div className="flex flex-wrap items-end justify-between gap-2 py-4">
+     <div className="flex gap-2">
+      
+     </div>
+     <div className="flex flex-warp gap-2">
+      <DataTableViewOptions table={table} />
+     </div>
+    </div>
      <SkeletonWrapper isLoading={history.isFetching} fullwidth={false}> 
 <div className="rounded-md border">
       <Table>
@@ -184,6 +236,24 @@ function TransactionsTable({ from, to }: Props) {
         </TableBody>
       </Table>
     </div>
+    <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </SkeletonWrapper>
    </div>
   );
